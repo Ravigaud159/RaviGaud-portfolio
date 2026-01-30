fed3557f5f3cfd4d42e810aec723b9a3010bfd1e
Ravigaud159 <ravigaud159@gmail.com>
2026-01-30 23:05:16 +0530

    Initial commit - Review Submission System

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -24,6 +24,7 @@ import projectImg3 from "../assets/TASKMANGER.png";
 import projectImg4 from "../assets/image.png";
 import projectImg5 from "../assets/tourism.png";
 import projectImg6 from "../assets/Digi.png";
+import projectImg7 from "../assets/Review.png";
 
 export const assets = {
   profileImg,
@@ -131,12 +132,11 @@ export const projects = [
       "CSS3",
       "JavaScript",
       "Google Gemini AI",
-    "Emoji Support (EmojiMart)",
+      "Emoji Support (EmojiMart)",
     ],
     icons: [
       FaHtml5, // HTML5
       FaCss3Alt, // CSS3
-      
     ],
     demo: "https://github.com/Ravigaud159/AICHATBOT",
     code: "https://github.com/Ravigaud159/AICHATBOT.git",
@@ -147,31 +147,31 @@ export const projects = [
       "A fully responsive web platform that allows users to filter doctors by specialty and book appointments seamlessly. Features include admin and doctor dashboards for managing profiles, schedules, and appointments, with secure backend integration using Node.js, Express.js, MongoDB, and JWT authentication.",
     image: projectImg2,
     tech: [
-    "React.js",
-    "Tailwind CSS",
-    "JavaScript",
-    "Node.js",
-    "Express.js",
-    "MongoDB",
-    "REST APIs",
-    "JWT Authentication",
-    "Git",
-    "GitHub",
-    "Postman",
-  ],
-     icons: [
-    FaReact,     // React.js
-    FaTools,     // Tailwind CSS / JavaScript
-    FaCode,      // JavaScript
-    FaNodeJs,    // Node.js
-    FaServer,    // Express.js / REST APIs
-    FaDatabase,  // MongoDB
-    FaServer,    // REST APIs    // JWT Authentication
-    FaTools,     // Git
-    FaTools,     // GitHub
-    FaTools,     // Postman
-  ],
-    demo: "https://github.com/Ravigaud159/OnlineDoctorAppointment",
+      "React.js",
+      "Tailwind CSS",
+      "JavaScript",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "REST APIs",
+      "JWT Authentication",
+      "Git",
+      "GitHub",
+      "Postman",
+    ],
+    icons: [
+      FaReact, // React.js
+      FaTools, // Tailwind CSS / JavaScript
+      FaCode, // JavaScript
+      FaNodeJs, // Node.js
+      FaServer, // Express.js / REST APIs
+      FaDatabase, // MongoDB
+      FaServer, // REST APIs    // JWT Authentication
+      FaTools, // Git
+      FaTools, // GitHub
+      FaTools, // Postman
+    ],
+    demo: "https://doctor-booking-appointment-zeta.vercel.app/",
     code: "https://github.com/Ravigaud159/OnlineDoctorAppointment.git",
   },
   {
@@ -180,24 +180,24 @@ export const projects = [
       "A scalable full-stack web application built with React.js, TailwindCSS, and Node.js/Express. Users can register, login, and manage tasks through a responsive dashboard, with search & filter functionality and secure JWT-based authentication.",
     image: projectImg3,
     tech: [
-    "React.js",
-    "Tailwind CSS",
-    "Node.js",
-    "Express.js",
-    "MongoDB Atlas",
-    "JWT Authentication",
-    "bcrypt",
-  ],
-   icons: [
-    FaReact,     // React.js
-    FaCss3Alt,   // Tailwind CSS
-    FaNodeJs,    // Node.js
-    FaServer,    // Express.js
-    FaDatabase,  // MongoDB Atlas    // JWT Authentication
-    FaTools,     // bcrypt / security
-  ],
+      "React.js",
+      "Tailwind CSS",
+      "Node.js",
+      "Express.js",
+      "MongoDB Atlas",
+      "JWT Authentication",
+      "bcrypt",
+    ],
+    icons: [
+      FaReact, // React.js
+      FaCss3Alt, // Tailwind CSS
+      FaNodeJs, // Node.js
+      FaServer, // Express.js
+      FaDatabase, // MongoDB Atlas    // JWT Authentication
+      FaTools, // bcrypt / security
+    ],
 
-    demo: "https://github.com/Ravigaud159/Task-Manager-Dashboard-Web-App",
+    demo: "https://task-manger-dashboard-frotend.vercel.app/",
     code: "https://github.com/Ravigaud159/Task-Manager-Dashboard-Web-App.git",
   },
   {
@@ -205,24 +205,24 @@ export const projects = [
     description:
       "A web-based application developed using C# and the .NET Framework with ASP.NET Web Forms, designed to securely manage and store student records. The system integrates SQL Server for persistent data storage and features a clean, modular UI enhanced through Master Pages and AWT components.",
     image: projectImg4,
-     tech: [
-    "C#",
-    "ASP.NET Web Forms",
-    "AWT",
-    "Master Pages",
-    "SQL Server",
-    "Visual Studio",
-    ".NET Framework",
-  ],
+    tech: [
+      "C#",
+      "ASP.NET Web Forms",
+      "AWT",
+      "Master Pages",
+      "SQL Server",
+      "Visual Studio",
+      ".NET Framework",
+    ],
     icons: [
-    FaCode,       // C# / coding
-    FaServer,     // ASP.NET backend
-     // Web Forms / frontend UI
-    FaDatabase,   // SQL Server
-    FaTools,      // Visual Studio / Development Tools
+      FaCode, // C# / coding
+      FaServer, // ASP.NET backend
+      // Web Forms / frontend UI
+      FaDatabase, // SQL Server
+      FaTools, // Visual Studio / Development Tools
       // Authentication / Security
-        // Master Pages / modular UI elements
-  ],
+      // Master Pages / modular UI elements
+    ],
     demo: "https://github.com/Ravigaud159/-Student-Information-System",
     code: "https://github.com/Ravigaud159/-Student-Information-System.git",
   },
@@ -232,20 +232,31 @@ export const projects = [
       "The Tourism Management System is a frontend-focused web application developed as part of my bridge course project. It simulates a tourism portal where users can log in, explore tour and hotel packages, and view or submit reviews. The project emphasizes clean UI design, usability, and core frontend concepts using vanilla web technologies. It demonstrates how a real-world tourism platform can be structured and made interactive without backend integration.",
     image: projectImg5,
     tech: ["HTML", "CSS", "JavaScript"],
-     icons: [FaHtml5, FaCss3Alt],
+    icons: [FaHtml5, FaCss3Alt],
 
-    demo: "https://github.com/Ravigaud159/-Tourism-Management-System",
+    demo: "https://tourism-management-system-project.vercel.app/",
     code: "https://github.com/Ravigaud159/-Tourism-Management-System.git",
   },
   {
     title: "Digital Clock Web App",
     description:
-  "A simple and stylish digital clock built using HTML, CSS, and JavaScript. It displays the current time in hours, minutes, and seconds with real-time updates every second, featuring a modern glassmorphism-inspired UI with blur and gradient effects.",  
+      "A simple and stylish digital clock built using HTML, CSS, and JavaScript. It displays the current time in hours, minutes, and seconds with real-time updates every second, featuring a modern glassmorphism-inspired UI with blur and gradient effects.",
     image: projectImg6,
     tech: ["HTML", "CSS", "JavaScript"],
-icons: [FaHtml5, FaCss3Alt],
+    icons: [FaHtml5, FaCss3Alt],
     demo: "https://github.com/Ravigaud159/DigitalClock",
     code: "https://github.com/Ravigaud159/DigitalClock.git",
+  },
+
+  {
+    title: "Review Submission System",
+    description:
+      "A simple and user-friendly Review Submission System built using HTML, CSS, and JavaScript. Users can submit reviews with a username, rating (1–5), and comments. All reviews are stored in the browser’s localStorage and displayed instantly on the page with an auto-generated timestamp. The data persists even after refreshing the browser, making it a fully client-side solution.",
+    image: projectImg7, // change if needed
+    tech: ["HTML", "CSS", "JavaScript", "LocalStorage"],
+    icons: [FaHtml5, FaCss3Alt],
+    demo: "https://review-system-livid.vercel.app/",
+    code: "https://github.com/Ravigaud159/Review_System?tab=readme-ov-file",
   },
 ];
 
@@ -266,5 +277,4 @@ export const workData = [
       "Designed and developed multiple frontend web applications as part of academic coursework and personal learning. Built responsive and user-friendly interfaces using HTML, CSS, JavaScript, React, and Tailwind CSS. Implemented features like authentication UI, dashboards, form validation, and API integration in project-based learning. Focused on clean UI, component reusability, and performance optimization. Collaborated with classmates on group projects, following basic Git/GitHub workflows.",
     color: "pink",
   },
-
 ];
